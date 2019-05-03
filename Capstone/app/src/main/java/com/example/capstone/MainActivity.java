31b2834db37a60fe1a48e85396d68efb44f524a4
yeonwook1993 <yeonwook1993@gmail.com>
2019-05-03 16:17:20 +0900

    19-05-03

--- Capstone/app/src/main/java/com/example/capstone/MainActivity.java
+++ Capstone/app/src/main/java/com/example/capstone/MainActivity.java
@@ -4,20 +4,22 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
 
-        setContentView(R.layout.activity_main);
-        setContentView(R.layout.activity_main);
 
         Button b1 = (Button)findViewById(R.id.button);
         Button b2 = (Button)findViewById(R.id.button2);
-
 
         //b1의 리스너
         b1.setOnClickListener(new View.OnClickListener(){
--- Capstone/app/src/main/java/com/example/capstone/activeVerify.java
+++ Capstone/app/src/main/java/com/example/capstone/activeVerify.java
@@ -2,12 +2,15 @@ package com.example.capstone;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class activeVerify extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_active_verify);
     }
 }
