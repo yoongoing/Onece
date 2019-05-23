3d7e295135dd9ed0d22ff356dd50fca62d92e72d
orgensthe <w4102945@gmail.com>
2019-05-23 18:23:35 +0900

    Push system almost done

--- Capstone/app/src/main/java/com/example/capstone/FirebaseInstanceIDService.java
+++ Capstone/app/src/main/java/com/example/capstone/FirebaseInstanceIDService.java
@@ -24,6 +24,8 @@ public class FirebaseInstanceIDService extends FirebaseMessagingService {
     public void onNewToken(String s) {
         super.onNewToken(s);
         Log.e("Firebase", "FirebaseInstanceIDService : " + s);
+        System.out.println(s+"asjdflaksdhfalksjdhflkasjhdflaksjfhl");
+
     }
 
     /**
--- Capstone/app/src/main/java/com/example/capstone/MainActivity.java
+++ Capstone/app/src/main/java/com/example/capstone/MainActivity.java
@@ -3,17 +3,23 @@ package com.example.capstone;
 import android.content.ContentValues;
 import android.content.Intent;
 import android.os.AsyncTask;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.iid.FirebaseInstanceIdService;
-
+import com.google.firebase.iid.InstanceIdResult;
+import com.google.firebase.messaging.FirebaseMessagingService;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -44,10 +50,17 @@ public class MainActivity extends AppCompatActivity {
                 String ip = "http://192.168.1.72:7777/?id=";
                 String url = ip.concat(idData);
                
-                String token = FirebaseInstanceId.getInstance().getInstanceId().toString();
+                String token =FirebaseInstanceId.getInstance().getInstanceId().addOnSuccessListener( MainActivity.this,  new OnSuccessListener<InstanceIdResult>() {
+                    @Override
+                    public void onSuccess(InstanceIdResult instanceIdResult) {
+                        String newToken = instanceIdResult.getToken();
+                        Log.e("newToken",newToken);
 
+                    }
+                }).toString();
+                System.out.println(token);
 
-                String temp = "http://192.168.0.26:9000/?method=r&id=testId&publickey=1234&token="+token;
+                String temp = "http://192.168.0.116:9000/?method=r&id=testId&publickey=1234&token="+token;
                 NetworkTask networkTask = new NetworkTask(temp, null);
                 networkTask.execute();
                 
