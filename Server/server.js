f98b42827c38a01734a4ab4ebddd3f006c809912
orgensthe <w4102945@gmail.com>
2019-05-29 18:44:25 +0900

    fix public key-12

--- Server/server.js
+++ Server/server.js
@@ -245,9 +245,11 @@ var app = http.createServer((request, response) => {
 
 		checkIdAndName();
 		getCommand = getCommand1 + userId + getCommand2 ;
-		
+
 		exec1(getCommand, function (err, stdout, stderr) {
 			userPublicKey = stdout.toString();
+			console.log(userPublicKey);
+			console.log("sibalnoma whe andoenenguya")
 		});
 		
 		setTimeout( sendmessage,2000);
