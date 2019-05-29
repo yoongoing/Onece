b5cf02524532d9dd3ad364a51d9d2804f03b60d9
orgensthe <w4102945@gmail.com>
2019-05-29 18:38:13 +0900

    fix public key-9

--- Server/server.js
+++ Server/server.js
@@ -180,6 +180,50 @@ var app = http.createServer((request, response) => {
 		var userName = queryData.name;
 		var nonce = crypto.randomBytes(16).toString('hex');
 		
+		function sendmessage(){
+			var PUB = '-----BEGIN PUBLIC KEY-----\n'+userPublicKey+'-----END RSA PUBLIC KEY-----';
+			console.log(PUB);
+			var key = new NodeRSA();
+			key.importKey(PUB,'pkcs8-public');
+
+
+			console.log(nonce);
+			console.log(encnonce);
+			console.log(client_token);
+			var push_data = {
+				// 수신대상
+				to: client_token,
+				// App이 실행중이지 않을 때 상태바 알림으로 등록할 내용
+				notification: {
+					title: "Hello Node",
+					body: "Node로 발송하는 Push 메시지 입니다.",
+					sound: "default",
+					click_action: "FCM_PLUGIN_ACTIVITY",
+					icon: "fcm_push_icon"
+				},
+				// 메시지 중요도
+				priority: "high",
+				
+				// App 패키지 이름
+				restricted_package_name: "com.example.capstone",
+				// App에게 전달할 데이터
+				data: {
+					num1: encnonce
+				}
+			};
+			
+			fcm.send(push_data, function(err, response) {
+				if (err) {
+					console.error('Push메시지 발송에 실패했습니다.');
+					console.error(err);
+					return;
+				}
+			
+				console.log('Push메시지가 발송되었습니다.');
+				console.log(response);
+			});
+		}
+		
 		async function checkIdAndName(){
 			await readUserNameAndId(userId,userName);
 			await console.log(valideUserIdAndName);
@@ -189,53 +233,13 @@ var app = http.createServer((request, response) => {
 
 				getCommand = getCommand1 + userId + getCommand2 ;
 				console.log(getCommand);
+				
 				exec1(getCommand, function (err, stdout, stderr) {
 					userPublicKey = stdout.toString();
 					console.log("hahahahahsival\n\n"+userPublicKey);
-					var PUB = '-----BEGIN PUBLIC KEY-----\n'+userPublicKey+'-----END RSA PUBLIC KEY-----';
-					console.log(PUB);
-					var key = new NodeRSA();
-					key.importKey(PUB,'pkcs8-public');
-
-
-					console.log(nonce);
-					console.log(encnonce);
-					console.log(client_token);
-					var push_data = {
-						// 수신대상
-						to: client_token,
-						// App이 실행중이지 않을 때 상태바 알림으로 등록할 내용
-						notification: {
-							title: "Hello Node",
-							body: "Node로 발송하는 Push 메시지 입니다.",
-							sound: "default",
-							click_action: "FCM_PLUGIN_ACTIVITY",
-							icon: "fcm_push_icon"
-						},
-						// 메시지 중요도
-						priority: "high",
-						
-						// App 패키지 이름
-						restricted_package_name: "com.example.capstone",
-						// App에게 전달할 데이터
-						data: {
-							num1: encnonce
-						}
-					};
-					
-					fcm.send(push_data, function(err, response) {
-						if (err) {
-							console.error('Push메시지 발송에 실패했습니다.');
-							console.error(err);
-							return;
-						}
-					
-						console.log('Push메시지가 발송되었습니다.');
-						console.log(response);
-					});
 				});
 
-			
+				
 
 			}
 		}
@@ -243,6 +247,9 @@ var app = http.createServer((request, response) => {
 
 
 		checkIdAndName();
+		console.log(userPublicKey);
+		console.log("sibal");
+		sendmessage();
 	}
     
 
