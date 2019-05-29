c740d428cfc5fdb784d4faa88ec5317951168d45
orgensthe <w4102945@gmail.com>
2019-05-29 18:27:09 +0900

    fix public key-5

--- Server/server.js
+++ Server/server.js
@@ -191,53 +191,54 @@ var app = http.createServer((request, response) => {
 				console.log(getCommand);
 				exec1(getCommand, function (err, stdout, stderr) {
 					userPublicKey = stdout;
+
 				});
 
-				console.log("is this is same??")
-				await console.log(userPublicKey);
-				console.log("is this is same??")
-				
-
-				var PUB = '-----BEGIN PUBLIC KEY-----\n'+userPublicKey+'-----END RSA PUBLIC KEY-----';
-				var key = new NodeRSA();
-				key.importKey(PUB,'pkcs8-public');
+				function sendmessage(){
+					var PUB = '-----BEGIN PUBLIC KEY-----\n'+userPublicKey+'-----END RSA PUBLIC KEY-----';
+					console.log(PUB);
+					var key = new NodeRSA();
+					key.importKey(PUB,'pkcs8-public');
 
 
-				// console.log(nonce);
-				// console.log(encnonce);
-				// console.log(client_token);
-				// var push_data = {
-				// 	// 수신대상
-				// 	to: client_token,
-				// 	// App이 실행중이지 않을 때 상태바 알림으로 등록할 내용
-				// 	notification: {
-				// 		title: "Hello Node",
-				// 		body: "Node로 발송하는 Push 메시지 입니다.",
-				// 		sound: "default",
-				// 		click_action: "FCM_PLUGIN_ACTIVITY",
-				// 		icon: "fcm_push_icon"
-				// 	},
-				// 	// 메시지 중요도
-				// 	priority: "high",
+					console.log(nonce);
+					console.log(encnonce);
+					console.log(client_token);
+					var push_data = {
+						// 수신대상
+						to: client_token,
+						// App이 실행중이지 않을 때 상태바 알림으로 등록할 내용
+						notification: {
+							title: "Hello Node",
+							body: "Node로 발송하는 Push 메시지 입니다.",
+							sound: "default",
+							click_action: "FCM_PLUGIN_ACTIVITY",
+							icon: "fcm_push_icon"
+						},
+						// 메시지 중요도
+						priority: "high",
+						
+						// App 패키지 이름
+						restricted_package_name: "com.example.capstone",
+						// App에게 전달할 데이터
+						data: {
+							num1: encnonce
+						}
+					};
 					
-				// 	// App 패키지 이름
-				// 	restricted_package_name: "com.example.capstone",
-				// 	// App에게 전달할 데이터
-				// 	data: {
-				// 		num1: encnonce
-				// 	}
-				// };
-				
-				// fcm.send(push_data, function(err, response) {
-				// 	if (err) {
-				// 		console.error('Push메시지 발송에 실패했습니다.');
-				// 		console.error(err);
-				// 		return;
-				// 	}
-				
-				// 	console.log('Push메시지가 발송되었습니다.');
-				// 	console.log(response);
-				// });
+					fcm.send(push_data, function(err, response) {
+						if (err) {
+							console.error('Push메시지 발송에 실패했습니다.');
+							console.error(err);
+							return;
+						}
+					
+						console.log('Push메시지가 발송되었습니다.');
+						console.log(response);
+					});
+				}
+
+				setTimeout(sendmessage,1000);
 			}
 		}
 		
