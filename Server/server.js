8f418cbfa00435a10e0442f673c2f563caf5a950
orgensthe <w4102945@gmail.com>
2019-06-01 01:00:55 +0900

    encryption test-61

--- Server/server.js
+++ Server/server.js
@@ -220,7 +220,7 @@ var app = http.createServer((request, response) => {
 			
 			var base64Nonce = hexToBase64(nonce);
 
-			PUB = PUB.toString('base64');
+			
 			var buffer = str2ab(base64String);
 			var encnonce  = crypto.publicEncrypt({ key: PUB, padding: crypto.constants.RSA_PKCS1_PADDING }, Buffer.from(base64Nonce,'base64'));
 
